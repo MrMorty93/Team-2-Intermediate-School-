10f7fc135b93d26a004fbdd30c85081480110dd7
Johnathen Morton <jmorton73792@mymail.tstc.edu>
2022-05-31 10:15:41 -0500

    Created main login form and opened and closed the database

---
+++ Team2-IntermediateSchool/clsSQL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Team2_IntermediateSchool
+{
+    internal class clsSQL
+    {
+        const string strSchema = "group2su222330.";
+        const string strConnectionString = "Server=cstnt.tstc.edu; Database=inew2330su22; User Id=group2su222330; Password=2856454";
+
+        private static SqlConnection _cntDatabase = new SqlConnection(strConnectionString);
+
+        public static void OpenDatabase()
+        {
+            _cntDatabase.Open();
+        }
+
+        public static void CloseDatabase()
+        {
+            _cntDatabase.Close();
+        }
+    }
+}
